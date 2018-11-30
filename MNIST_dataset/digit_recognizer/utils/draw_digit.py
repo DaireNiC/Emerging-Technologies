ee86e6c9ca7e74c132eb1ca2d73714ddfa09535b
Daire Ni Chathain <dairenichat@gmail.com>
2018-11-30 21:08:46 +0000

    pep8

--- MNIST_dataset/digit_recognizer/utils/draw_digit.py
+++ MNIST_dataset/digit_recognizer/utils/draw_digit.py
@@ -1,6 +1,9 @@
-# Adapted from: ()
-# https://stackoverflow.com/questions/2498875/how-to-invert-colors-of-image-with-pil-python-imaging
-# https://stackoverflow.com/questions/52146562/python-tkinter-paint-how-to-paint-smoothly-and-save-images-with-a-different
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Author: Daire Ní  Chathain
+# Adapted from:
+#(1):  https://stackoverflow.com/questions/2498875/how-to-invert-colors-of-image-with-pil-python-imaging
+#(2): https://stackoverflow.com/questions/52146562/python-tkinter-paint-how-to-paint-smoothly-and-save-images-with-a-different
 
 from tkinter import *
 import PIL
@@ -9,43 +12,47 @@ from PIL import Image, ImageDraw, ImageOps
 
 def save():
     global image_number
-    size = 28,28
+    size = (28, 28)
     image1.thumbnail(size, Image.ANTIALIAS)
     image_save = PIL.ImageOps.invert(image1)
-    filename = 'user_img.png'   # image_number increments by 1 at every save
+    filename = 'user_img.png'  # image_number increments by 1 at every save
     image_save.save(filename)
 
 
 def activate_paint(e):
     global lastx, lasty
     cv.bind('<B1-Motion>', paint)
-    lastx, lasty = e.x, e.y
+    (lastx, lasty) = (e.x, e.y)
 
 
 def paint(e):
     global lastx, lasty
-    x, y = e.x, e.y
+    (x, y) = (e.x, e.y)
     cv.create_line((lastx, lasty, x, y), width=10)
+
     #  --- PIL
+
     draw.line((lastx, lasty, x, y), fill='black', width=10)
-    lastx, lasty = x, y
+    (lastx, lasty) = (x, y)
 
 
 def open_canvas():
     global cv, draw, image1, lastx, lasty
     root = Tk()
 
-    lastx, lasty = None, None
+    (lastx, lasty) = (None, None)
 
     cv = Canvas(root, width=300, height=300, bg='white')
+
     # --- PIL
+
     image1 = PIL.Image.new('LA', (300, 300), 'white')
     draw = ImageDraw.Draw(image1)
 
     cv.bind('<1>', activate_paint)
     cv.pack(expand=YES, fill=BOTH)
 
-    btn_save = Button(text="save", command=save)
+    btn_save = Button(text='save', command=save)
     btn_save.pack()
 
     root.mainloop()
--- MNIST_dataset/digitrec.py
+++ MNIST_dataset/digitrec.py
@@ -13,35 +13,44 @@ import argparse
 from utils.draw_image import *
 # import numpy
 import numpy as np
-from keras.datasets import mnist # No need to re-invent the wheel
+from keras.datasets import mnist  # No need to re-invent the wheel
 from keras.utils import np_utils
 from keras.models import load_model
 
 from PIL import Image
 # constants
-NUM_CLASSES = 10 # represents 0-9 digits
+NUM_CLASSES = 10  # represents 0-9 digits
 # where the model should be stored
 MODEL_NAME = 'keras_mnist.h5'
 
 """
 This function loads the MNIST dataset and preforms basic preprocessing on it
 """
+
+
 def prep_mnist_data():
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
     # building the input vector from the 28x28 pixels
     # in each dataset we use numpy to invert the values - so that the background is white and didgit is black
     # normalizing the data to speed up training
-    X_train = ~np.array(list(X_train)).reshape(60000, 784).astype(np.uint8) /255
+    X_train = ~np.array(
+        list(X_train)).reshape(
+        60000,
+        784).astype(
+            np.uint8) / 255
     X_test = ~np.array(list(X_test)).reshape(10000, 784).astype(np.uint8) / 255
 
     return X_train, y_train, X_test, y_test
+
 
 """
 This function builds and trains a neural network using Keras and the MNIST dataset.
     INPUT: MNIST dataset --> tuple containing test & train images
     OUTPUT: Keras Sequential model
 """
+
+
 def build_neural_net(MNIST_data):
     # unpack MNIST data
     X_train, y_train, X_test, y_test = MNIST_data
@@ -50,46 +59,71 @@ def build_neural_net(MNIST_data):
     Y_test = np_utils.to_categorical(y_test, NUM_CLASSES)
     # Start a neural network, building it by layers.
     model = kr.models.Sequential()
-    # Add a hidden layer with 600 neurons and an input layer with 784( 28 x 28 pixles)
+    # Add a hidden layer with 600 neurons and an input layer with 784( 28 x 28
+    # pixles)
     model.add(kr.layers.Dense(units=600, activation='linear', input_dim=784))
     # We then stack another layer, this time one of 400 neurons.
     model.add(kr.layers.Dense(units=400, activation='relu'))
     # Add a ten neuron output layer to represent digits 0-9
     model.add(kr.layers.Dense(units=NUM_CLASSES, activation='softmax'))
     # Build the graph.
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy'])
     # train the neural net
-    model.fit(X_train, Y_train,batch_size=120, epochs=1,verbose=2,validation_data=(X_test, Y_test))
+    model.fit(
+        X_train,
+        Y_train,
+        batch_size=120,
+        epochs=1,
+        verbose=2,
+        validation_data=(
+            X_test,
+            Y_test))
     # evaluate score and print
     score = model.evaluate(X_test, Y_test, verbose=1)
     print(type(score[1]))
-    print ('Successfully created model with ' +  score[1].astype(str) + ' accuracy')
+    print('Successfully created model with ' +
+          score[1].astype(str) + ' accuracy')
     return model
+
 
 """
 This function takes in an image and classifies the image using the neural network
     INPUT: Image file
 """
+
+
 def predict_img(input_img):
     # Returns a compiled model
     model = load_model(MODEL_NAME)
     print('Classifying digit ...')
     # parsing the image to meet model input structure
     #img = np.invert(Image.open("test_img.png")).reshape(1,784)
-    img = np.invert(Image.open(input_img)).reshape(1,784)
+    img = np.invert(Image.open(input_img)).reshape(1, 784)
     # get the corresponding categorical value from the hot encoded array
     # changes from binary array --> gets max (where the position of the 1 is)
-    print("Predicted: " + np.argmax(model.predict(img), axis=None, out=None).astype(str) )
+    print(
+        "Predicted: " +
+        np.argmax(
+            model.predict(img),
+            axis=None,
+            out=None).astype(str))
+
 
 """
 This function takes in the keras sequential model and saves it to current dir
     INPUT: Keras Sequential model
 """
+
+
 def save_model(model):
     # saving the model)
     model_path = os.path.join(MODEL_NAME)
     model.save(MODEL_NAME)
     print('Saved model as ' + MODEL_NAME)
+
 
 def get_img(x):
     """
@@ -101,16 +135,21 @@ def get_img(x):
         raise argparse.ArgumentTypeError("{0} does not exist".format(x))
     return x
 
+
 if __name__ == '__main__':
 
     utils.draw_image.open_canvas()
     # parse the command line arguments
     parser = argparse.ArgumentParser(description="digit recogniser")
-    parser.add_argument("-i", "--input",
-        dest="filename", required=True, type=get_img,
-        help="Path to image you wish to classify", metavar="FILE")
+    parser.add_argument(
+        "-i",
+        "--input",
+        dest="filename",
+        required=True,
+        type=get_img,
+        help="Path to image you wish to classify",
+        metavar="FILE")
     args = parser.parse_args()
-
 
     # if model exists already don't create a new one
     if (os.path.isfile(MODEL_NAME)):
